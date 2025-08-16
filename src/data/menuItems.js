98837eb8d0e725bbfd42c732c5d92b8ca1b355ec
Rafael <rafaelgasparmartins@icloud.com>
2025-08-16 09:16:34 -0300

    Adiciona arquivo menuItems.js com itens do menu de navegação

---
+++ src/data/menuItems.js
@@ -0,0 +1,8 @@
+export const menuItems = [
+  { id: "home", label: "Início" },
+  { id: "sobre", label: "Sobre Mim" },
+  { id: "skills", label: "Skills" },
+  { id: "projetos", label: "Projetos" },
+  { id: "certificados", label: "Certificados" },
+  { id: "contato", label: "Contato" },
+];
