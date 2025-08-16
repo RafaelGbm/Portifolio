c0ffb4b7eb0df1e05ccd3fa7f9516bf737f96971
Rafael <rafaelgasparmartins@icloud.com>
2025-08-16 09:16:24 -0300

    Adiciona arquivo certifications.js com dados de certificados

---
+++ src/data/certifications.js
@@ -0,0 +1,7 @@
+
+
+export const certifications = [
+  { icon: null, title: "Engenharia de Software - FIAP", year: "2024 - 2027" },
+  { icon: null, title: "Big Data & Analytics", year: "2023" },
+  { icon: null, title: "Blockchain Essentials", year: "2023" },
+];
