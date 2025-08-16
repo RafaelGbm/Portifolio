819b80848a308bfc986c99860cac86804df5a326
Rafael <rafaelgasparmartins@icloud.com>
2025-08-16 09:16:39 -0300

    Adiciona arquivo skills.js com dados de habilidades

---
+++ src/data/skills.js
@@ -0,0 +1,34 @@
+import git from "../assets/skills/git.svg";
+import github from "../assets/skills/github.svg";
+import html from "../assets/skills/html.svg";
+import css from "../assets/skills/css.svg";
+import java from "../assets/skills/java.webp";
+import bootstrap from "../assets/skills/bootstrap.svg";
+import react from "../assets/skills/react.svg";
+import javascript from "../assets/Skills/javascript-logo.png";
+import sass from "../assets/skills/sass.png";
+import python from "../assets/skills/python.png";
+import mysql from "../assets/skills/MySQL.png";
+import nodejs from "../assets/skills/nodejs.svg";
+import unreal from "../assets/skills/unreal.png";
+import c from "../assets/skills/C.png";
+
+
+
+
+export const skills = [
+  { icon: git, title: "Git", description: "Controle de versão" },
+  { icon: github, title: "GitHub", description: "Repositórios" },
+  { icon: html, title: "HTML", description: "Markup" },
+  { icon: css, title: "CSS", description: "Estilos" },
+  { icon: java, title: "Java", description: "Backend" },
+  { icon: bootstrap, title: "Bootstrap", description: "Frontend" },
+  { icon: react, title: "React", description: "Frontend" },
+  { icon: javascript, title: "JavaScript", description: "Frontend" },
+  { icon: sass, title: "Sass", description: "Estilos" },
+  { icon: python, title: "Python", description: "Backend" },
+  { icon: mysql, title: "MySQL", description: "Banco de Dados" },
+  { icon: nodejs, title: "Node.js", description: "Backend" },
+  { icon: unreal, title: "Unreal Engine", description: "Game Development" },
+  { icon: c, title: "C#", description: "Backend" },
+];
