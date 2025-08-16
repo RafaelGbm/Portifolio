f23f9643f7ed24db27557fb4c62f28073e41af7f
Rafael Duarte <rafa.duartef@outlook.com>
2025-08-16 16:52:55 -0300

    correção das imagens

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,20 +1,17 @@
-import git from "../assets/skills/git.svg";
-import github from "../assets/skills/github.svg";
-import html from "../assets/skills/html.svg";
-import css from "../assets/skills/css.svg";
-import java from "../assets/skills/java.webp";
-import bootstrap from "../assets/skills/bootstrap.svg";
-import react from "../assets/skills/react.svg";
+import git from "../assets/Skills/git.svg";
+import github from "../assets/Skills/github.svg";
+import html from "../assets/Skills/html.svg";
+import css from "../assets/Skills/css.svg";
+import java from "../assets/Skills/java.webp";
+import bootstrap from "../assets/Skills/bootstrap.svg";
+import react from "../assets/Skills/react.svg";
 import javascript from "../assets/Skills/javascript-logo.png";
-import sass from "../assets/skills/sass.png";
-import python from "../assets/skills/python.png";
-import mysql from "../assets/skills/MySQL.png";
-import nodejs from "../assets/skills/nodejs.svg";
-import unreal from "../assets/skills/unreal.png";
-import c from "../assets/skills/C.png";
-
-
-
+import sass from "../assets/Skills/sass.png";
+import python from "../assets/Skills/python.png";
+import mysql from "../assets/Skills/MySQL.png";
+import nodejs from "../assets/Skills/nodejs.svg";
+import unreal from "../assets/Skills/unreal.png";
+import c from "../assets/Skills/C.png";
 
 export const skills = [
   { icon: git, title: "Git", description: "Controle de versão" },
